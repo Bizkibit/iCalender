a910e222bad30b21b474dd429d67a4f3aaa0005d
Farshid HM <fhmanesh@gmail.com>
2017-08-17 17:01:59 -0700

    changing months functionality has been added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,20 @@ import React from 'react'
 let MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 export class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e) {
+    let {target} = e;
+    target.preventDefault;
+    if (target.id === 'increase')
+      {this.props.onClick(1)}
+    else
+      {this.props.onClick(-1)}
+  }
+
   render()  {
     let style={backgroundColor: 'skyblue',
           display: 'flex',
@@ -14,9 +28,9 @@ export class Header extends React.Component {
 
     return(
       <div style={style}>
-        <i className="fa fa-arrow-circle-o-left fa-3x" aria-hidden="true"></i>
+        <i id='decrease' className="fa fa-arrow-circle-o-left fa-3x" aria-hidden="true" onClick={this.handleClick}></i>
         <h2>{MONTHS[this.props.month]} {this.props.year}</h2>
-        <i className="fa fa-arrow-circle-o-right fa-3x" aria-hidden="true"></i>
+        <i id='increase' className="fa fa-arrow-circle-o-right fa-3x" aria-hidden="true" onClick={this.handleClick}></i>
       </div>
     )
   }
--- src/components/Icalender.js
+++ src/components/Icalender.js
@@ -3,9 +3,25 @@ import {Header} from './Header'
 import {Body} from './Body'
 
 export class Icalender extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectDate: this.props.selectDate
+    }
+    this.handleMonthChange = this.handleMonthChange.bind(this)
+  }
+
+  handleMonthChange(i) {
+    let {selectDate} = this.state;
+    selectDate = new Date(selectDate)
+    debugger
+    this.setState({
+      selectDate: new Date(selectDate.getFullYear(), selectDate.getMonth()+i, selectDate.getDate())
+    })
+  }
 
   render()  {
-    let {selectDate} = this.props;
+    let {selectDate} = this.state;
     selectDate = new Date(selectDate)
 
     let date = selectDate.getDate();
@@ -16,7 +32,7 @@ export class Icalender extends React.Component {
 
     return (
       <div style={{ border: '15px black solid', width: '500px' }}>
-        <Header month={month} year={year}/>
+        <Header month={month} year={year} onClick={this.handleMonthChange}/>
         <Body date={date} day={day} firstDay={firstDay.getDay()}/>
       </div>
     )
